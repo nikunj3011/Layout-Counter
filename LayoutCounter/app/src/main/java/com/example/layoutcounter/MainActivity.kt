868a55b05db662b828dab0a89fab25bf1f4c0b1a
Nikunj Rathod <nikunjrathod3011@gmail.com>
2021-02-01 02:50:00 -0400

    all done

--- LayoutCounter/app/src/main/java/com/example/layoutcounter/MainActivity.kt
+++ LayoutCounter/app/src/main/java/com/example/layoutcounter/MainActivity.kt
@@ -2,10 +2,55 @@ package com.example.layoutcounter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.layoutcounter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val countKey = "num"
+    private var count:Int= 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.apply {
+            countdownButton.setOnClickListener {sub()}
+            countupButton.setOnClickListener { add() }
+            toastButton.setOnClickListener { showtoast(it) }
+        }
+    }
+
+    private fun showtoast(view: View) {
+        Toast.makeText(applicationContext,
+            "Number: $count",
+            Toast.LENGTH_SHORT).show()
+    }
+
+    private fun add() {
+        count++;
+        update()
+    }
+    private fun sub() {
+        count--;
+        update()
+    }
+
+    private fun update(){
+        binding.apply {
+            textView?.text = count.toString()
+        }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState?.putInt(countKey,count)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val savedNum = savedInstanceState.getInt(countKey)
+        count = savedNum
+        update()
     }
 }
